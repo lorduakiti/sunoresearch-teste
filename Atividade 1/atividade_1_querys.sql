5ba3041eb4cfcce12b7745c83112546e7f8b9e9f
Uákiti Pires <lorduakiti@gmail.com>
2021-02-24 16:17:02 -0300

    Inclusão dos códigos da atividade 1

---
+++ Atividade 1/atividade_1_querys.sql
@@ -0,0 +1,31 @@
+﻿/* Consulta do funcionário com maior salário do departamento, não ordenada ou rankeada */
+SELECT
+  d.Name as "Department",
+  (SELECT
+    e2.Name
+  FROM Employee e2
+  WHERE e2.Salary = max(e.Salary)
+  LIMIT 1) as "Employee",
+  max(e.Salary) as "Salary"
+FROM Employee         e
+LEFT JOIN Departament d on d.Id = e.DepartmentId
+GROUP BY d.Name
+ORDER BY d.Name;
+
+
+/* Consulta de todos dos funcionários com maior salário do departamento */
+SELECT
+  d.Name as "Department",
+  (SELECT
+    array_agg(e2.Name)
+  FROM Employee e2
+  WHERE e2.Salary = max(e.Salary)) as "Employees",
+  (SELECT
+    count(*)
+  FROM Employee e3
+  WHERE e3.Salary = max(e.Salary)) as "QuantityEmployees",
+  max(e.Salary) as "Salary"
+FROM Employee         e
+LEFT JOIN Departament d on d.Id = e.DepartmentId
+GROUP BY d.Name
+ORDER BY d.Name;
---
+++ Atividade 1/atividade_1_schema.sql
@@ -0,0 +1,29 @@
+﻿/* Schema de tabelas */
+CREATE TABLE Departament (
+  Id serial,
+  Name varchar(25) NOT NULL,
+  PRIMARY KEY (Id),
+  UNIQUE (Name)
+);
+
+CREATE TABLE Employee (
+  Id serial,
+  Name varchar(50) NOT NULL,
+  Salary float(2) DEFAULT 0.00,
+  DepartmentId integer,
+  PRIMARY KEY (Id),
+  CONSTRAINT fk_employee_department_id FOREIGN KEY (DepartmentId) REFERENCES Departament (Id)
+);
+
+/* Inserção de registros */
+INSERT INTO Departament (Id, Name) VALUES 
+  (1, 'IT'),
+  (2, 'Sales');
+
+
+INSERT INTO Employee (Id, Name, Salary, DepartmentId) VALUES
+  (1, 'Joe', 70000, 1),
+  (2, 'Jim', 90000, 1),
+  (3, 'Henry', 80000, 2),
+  (4, 'Sam', 60000, 2),
+  (5, 'Max', 90000, 1);

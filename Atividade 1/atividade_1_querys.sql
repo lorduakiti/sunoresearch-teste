4112574ba3ec94ea50f3e673fa3c3ac69795c427
Uákiti Pires <lorduakiti@gmail.com>
2021-02-24 21:02:57 -0300

    Correção de erro conceitual de interpretação da Atividade 1

--- Atividade 1/atividade_1_querys.sql
+++ Atividade 1/atividade_1_querys.sql
@@ -1,31 +1,16 @@
 ﻿/* Consulta do funcionário com maior salário do departamento, não ordenada ou rankeada */
 SELECT
   d.Name as "Department",
-  (SELECT
-    e2.Name
-  FROM Employee e2
-  WHERE e2.Salary = max(e.Salary)
-  LIMIT 1) as "Employee",
-  max(e.Salary) as "Salary"
+  e.Name as "Employee",
+  e.Salary as "Salary"
 FROM Employee         e
-LEFT JOIN Departament d on d.Id = e.DepartmentId
-GROUP BY d.Name
-ORDER BY d.Name;
-
-
-/* Consulta de todos dos funcionários com maior salário do departamento */
-SELECT
-  d.Name as "Department",
-  (SELECT
-    array_agg(e2.Name)
-  FROM Employee e2
-  WHERE e2.Salary = max(e.Salary)) as "Employees",
-  (SELECT
-    count(*)
-  FROM Employee e3
-  WHERE e3.Salary = max(e.Salary)) as "QuantityEmployees",
-  max(e.Salary) as "Salary"
-FROM Employee         e
-LEFT JOIN Departament d on d.Id = e.DepartmentId
-GROUP BY d.Name
+RIGHT JOIN Departament d on d.Id = e.DepartmentId
+RIGHT JOIN (
+  SELECT
+    DepartmentId,    
+    max(Salary) as MaxSalary
+  FROM Employee
+  GROUP BY DepartmentId
+) as e2 on e2.DepartmentId = e.DepartmentId
+    AND e2.MaxSalary = e.Salary
 ORDER BY d.Name;
